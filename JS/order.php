ee64e784365522b9d124a6b8cf37e5cdf8705f1b
unknown <aimanafiqbinesam@gmail.com>
2021-12-12 17:09:57 +0800

    make it better

---
+++ JS/order.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'sql6.freemysqlhosting.net';
+$database = 'sql6458011';
+$user = 'sql6458011';
+$password = 'iiAemBn5sc';
+
+$dbconn = mysqli_connect($host, $user, $password, $database);
+
+$myEmail = $_POST['my-email'];
+$myName = $_POST['my-name'];
+$myLocation = $_POST['my-location'];
+$myOrder = $_POST['my-order'];
+
+$insert = "INSERT INTO Orders (emails, names, locations, orders) 
+VALUES ('$myEmail', '$myName', '$myLocation', '$myOrder')";
+$result = mysqli_query($dbconn, $insert)
+
+?>
---
+++ JS/serverdb.js
@@ -0,0 +1,32 @@
+function createForm() {
+  var my_form = document.createElement("form");
+  my_form.method = "post";
+  my_form.action = "order.php";
+
+  my_email = document.createElement("input");
+  my_email.type = "email";
+  my_email.name = "my-email";
+  my_email.value = document.getElementById("input-email");
+
+  my_name = document.createElement("input");
+  my_name.type = "text";
+  my_name.name = "my-name";
+  my_name.value = document.getElementById("input-name");
+
+  my_location = document.createElement("input");
+  my_location.type = "text";
+  my_location.name = "my-location";
+  my_location.value = document.getElementById("input-location");
+
+  my_order = document.createElement("input");
+  my_order.type = "text";
+  my_order.name = "my-order";
+  my_order.value = document.getElementById("input-order");
+
+  my_form.appendChild(my_email);
+  my_form.appendChild(my_name);
+  my_form.appendChild(my_location);
+  my_form.appendChild(my_order);
+
+  my_form.submit();
+}
