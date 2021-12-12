613bfc1322e1a60ad8bac8e84ce769982890abb7
unknown <aimanafiqbinesam@gmail.com>
2021-12-12 21:21:36 +0800

    make it better

--- JS/serverdb.js
+++ JS/serverdb.js
@@ -1,33 +1,54 @@
 function createForm() {
   var my_form = document.createElement("form");
+  //new one
+  my_form.method = "post";
+  my_form.action = "/https://formsubmit.co/05c22619658c8b6061476942f1a51a84";
+  /*
   my_form.method = "post";
   my_form.action = "JS/order.php";
-
+*/
   my_email = document.createElement("input");
   my_email.type = "email";
-  my_email.name = "my-email";
+  my_email.name = "email";
   my_email.value = document.getElementById("input-email").value;
 
   my_name = document.createElement("input");
   my_name.type = "text";
-  my_name.name = "my-name";
+  my_name.name = "name";
   my_name.value = document.getElementById("input-name").value;
 
   my_location = document.createElement("input");
   my_location.type = "text";
-  my_location.name = "my-location";
+  my_location.name = "Location";
   my_location.value = document.getElementById("input-location").value;
 
   my_order = document.createElement("input");
   my_order.type = "text";
-  my_order.name = "my-order";
+  my_order.name = "message";
   my_order.value = document.getElementById("input-order").value;
 
   my_form.appendChild(my_email);
   my_form.appendChild(my_name);
   my_form.appendChild(my_location);
   my_form.appendChild(my_order);
-  document.body.appendChild(my_form);
+  document.body.appendChild(my_form); //apenchild div lain
 
   my_form.submit();
+}
+
+function saveForm() {
+  document.getElementById("order-form").style.display = "block";
+  document.getElementById("input-email").disabled = "true";
+  document.getElementById("input-name").disabled = "true";
+  document.getElementById("input-location").disabled = "true";
+  document.getElementById("input-order").disabled = "true";
+}
+
+function editForm() {
+  //need to run form gk
+  document.getElementById("order-form").style.display = "none";
+  document.getElementById("input-email").disabled = "";
+  document.getElementById("input-name").disabled = "";
+  document.getElementById("input-location").disabled = "";
+  document.getElementById("input-order").disabled = "";
 }
