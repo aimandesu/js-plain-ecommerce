28413140349b4f6d4084f77bb49a694fe87421f3
unknown <aimanafiqbinesam@gmail.com>
2021-12-12 20:11:28 +0800

    make it better

--- JS/order.php
+++ JS/order.php
@@ -1,14 +1,14 @@
 <?php
-/*
+
 $host = 'sql6.freemysqlhosting.net';
 $database = 'sql6458011';
 $user = 'sql6458011';
 $password = 'iiAemBn5sc';
-*/
 
-//$dbconn = mysqli_connect($host, $user, $password, $database);
 
-$dbconn = mysqli_connect('localhost', 'root', '','studentlabbook');
+$dbconn = mysqli_connect($host, $user, $password, $database);
+
+//$dbconn = mysqli_connect('localhost', 'root', '','studentlabbook');
 
 $myEmail = $_POST['my-email'];
 $myName = $_POST['my-name'];
