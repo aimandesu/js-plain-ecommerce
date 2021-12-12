cfa6ae6ba076da37f457c1ea995dee27e3e3a3de
unknown <aimanafiqbinesam@gmail.com>
2021-12-12 17:25:16 +0800

    make it better

--- JS/serverdb.js
+++ JS/serverdb.js
@@ -6,22 +6,22 @@ function createForm() {
   my_email = document.createElement("input");
   my_email.type = "email";
   my_email.name = "my-email";
-  my_email.value = document.getElementById("input-email");
+  my_email.value = document.getElementById("input-email").value;
 
   my_name = document.createElement("input");
   my_name.type = "text";
   my_name.name = "my-name";
-  my_name.value = document.getElementById("input-name");
+  my_name.value = document.getElementById("input-name").value;
 
   my_location = document.createElement("input");
   my_location.type = "text";
   my_location.name = "my-location";
-  my_location.value = document.getElementById("input-location");
+  my_location.value = document.getElementById("input-location").value;
 
   my_order = document.createElement("input");
   my_order.type = "text";
   my_order.name = "my-order";
-  my_order.value = document.getElementById("input-order");
+  my_order.value = document.getElementById("input-order").value;
 
   my_form.appendChild(my_email);
   my_form.appendChild(my_name);
