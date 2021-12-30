b358e8cc545e10bbb4b12d3307497c33e75d0d68
unknown <aimanafiqbinesam@gmail.com>
2021-12-30 15:24:55 +0800

    make it better

---
+++ customer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  <style>
+    table,
+    th,
+    td {
+      border: 1px solid black;
+    }
+  </style>
+  </head>
+  <body>
+    <?php
+    $host = 'sql6.freemysqlhosting.net';
+    $database = 'sql6458011';
+    $user = 'sql6458011';
+    $password = 'iiAemBn5sc';
+    
+    
+    $dbconn = mysqli_connect($host, $user, $password, $database);
+    $sql = "SELECT 'No', names, phoneNo, locations, orders, prices FROM orders";
+    $result = $dbconn->query($sql);
+
+    if($result->num_rows > 0){
+      echo "<table>
+      <tr>
+        <th>No</th>
+        <th>Names</th>
+        <th>Phone No</th>
+        <th>Location</th>
+        <th>Order</th>
+        <th>Price</th>
+      </tr>";
+      while($row = $result->fetch_assoc()){
+        echo 
+        "<tr>
+        <td>".$row["No"]."</td>
+        <td>".$row["names"]."</td>
+        <td>".$row["phoneNo"]."</td>
+        <td>".$row["locations"]."</td>
+        <td>".$row["orders"]."</td>
+        <td>".$row["prices"]."</td></tr>";
+      }
+      echo "</table>";
+
+    }else{
+      echo "0 results";
+    }
+    $dbconn-> close();
+    ?>
+  </body>
+</html>
