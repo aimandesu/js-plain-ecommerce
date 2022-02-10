6045187a10e48846a6e199c71a7c5176b6049ef7
unknown <aimanafiqbinesam@gmail.com>
2022-02-10 09:56:43 +0800

    make it better

--- Product/JS/navMobile.js
+++ Product/JS/navMobile.js
@@ -1,9 +1,9 @@
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
-  document.getElementById("note").style.display = "none";
+  //document.getElementById("note").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
-  document.getElementById("note").style.display = "block";
+  //document.getElementById("note").style.display = "block";
 }
